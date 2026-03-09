44ba02cfade50f49bbe7584583254b1bb24f68cd
justinkasowski <justin.kasowski@gmail.com>
2026-03-08 22:58:08 -0700

    Immediate port availability for cloud build

--- services/api/start.sh
+++ services/api/start.sh
@@ -1,6 +1,9 @@
 ﻿#!/bin/bash
 set -e
 
+echo "Starting API..."
+uvicorn main:app --host 0.0.0.0 --port ${PORT}
+
 echo "Starting Ollama..."
 ollama serve &
 
@@ -10,5 +13,3 @@ sleep 5
 echo "Pulling model ${MODEL}"
 ollama pull ${MODEL}
 
-echo "Starting API..."
-uvicorn main:app --host 0.0.0.0 --port ${PORT}
